016b9db8f65b926697a8035c546598618209f0fe
Chuan Kai <chewchuankai@graduate.utm.my>
2026-05-06 23:12:46 +0800

    implement private route

--- client/src/App.js
+++ client/src/App.js
@@ -4,15 +4,28 @@ import Login from './pages/Login';
 import Dashboard from './pages/Dashboard';
 import IncidentList from './pages/IncidentList';
 import IncidentDetail from './pages/IncidentDetail';
+import ProtectedRoute from './components/ProtectedRoute';
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/login" element={<Login />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/incidents" element={<IncidentList />} />
-        <Route path="/incidents/:id" element={<IncidentDetail />} />
+        <Route path="/dashboard" element={
+          <ProtectedRoute>
+            <Dashboard />
+          </ProtectedRoute>
+        } />
+        <Route path="/incidents" element={
+          <ProtectedRoute>
+            <IncidentList />
+          </ProtectedRoute>
+        } />
+        <Route path="/incidents/:id" element={
+          <ProtectedRoute>
+            <IncidentDetail />
+          </ProtectedRoute>
+        } />
         {/* Redirect root to login by default */}
         <Route path="/" element={<Navigate to="/login" replace />} />
       </Routes>
